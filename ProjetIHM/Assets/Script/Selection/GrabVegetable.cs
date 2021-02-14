7af71e9eb76ee1edecdeb95c2771e1b48f41fe0b
TeoMouyebe <teo.mouyebe@efrei.net>
2021-02-14 20:12:39 +0100

    harvest management v1
    pas fou fou pour l'instant, c'est un début

---
+++ ProjetIHM/Assets/Script/Selection/GrabVegetable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabVegetable : MonoBehaviour
+{
+    private Transform _selection;
+    public GameObject player;
+    public GameObject crosshairBlue, crosshairBlanc;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material.shader = Shader.Find("Diffuse");
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag("Tomato") || selection.CompareTag("Cabbage"))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+                    if (Input.GetKeyDown("e"))
+                    {
+                        selection.gameObject.transform.position = new Vector3(-10.0f, 1f, 1.0f);
+                    }
+                }
+                _selection = selection;
+            }
+                
+        }
+    }
+}
--- ProjetIHM/Assets/Script/Selection/SelectionManager.cs
+++ ProjetIHM/Assets/Script/Selection/SelectionManager.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
-
     [SerializeField] private string selectableTag = "Selectable";
     private Transform _selection;
     public GameObject tomato,cabage;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -22,8 +22,10 @@ public class SelectionManager : MonoBehaviour
             selectionRenderer.material.shader = Shader.Find("Diffuse");
             _selection = null;
         }
+
         var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2f, Screen.height / 2f, 0f));
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit))
         {
             var selection = hit.transform;
@@ -39,16 +41,16 @@ public class SelectionManager : MonoBehaviour
                         if (selectionRenderer.gameObject.transform.parent.gameObject.name == "TomatoPlant")
                         {
                             GameObject fruit1 = Instantiate(tomato) as GameObject;
-                            fruit1.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(0f, 1f, 0f);
+                            fruit1.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(Random.Range(-0.4f, 0.4f), 1f, 0f);
                             GameObject fruit2 = Instantiate(tomato) as GameObject;
-                            fruit2.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(0f, 1.5f, 0f);
+                            fruit2.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(Random.Range(-0.4f, 0.4f), 1.5f, 0f);
                             GameObject fruit3 = Instantiate(tomato) as GameObject;
-                            fruit3.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(0f, 2f, 0f);
+                            fruit3.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(Random.Range(-0.4f, 0.4f), 2f, 0f);
                         }
                         else if (selectionRenderer.gameObject.transform.parent.gameObject.name == "Cabbages")
                         {
                             GameObject fruit1 = Instantiate(cabage) as GameObject;
-                            fruit1.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(0.1f, 1f, 0f);
+                            fruit1.transform.position = selectionRenderer.gameObject.transform.position + new Vector3(Random.Range(-0.4f,0.4f), 1f, 0f);
                         }
                     }
                 }
