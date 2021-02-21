107c2aa265a98ce5256d48ca5b666c2458fb2a5e
TeoMouyebe <teo.mouyebe@efrei.net>
2021-02-21 21:04:46 +0100

    GrowthManager final

--- ProjetIHM/Assets/Script/GrowthManager.cs
+++ ProjetIHM/Assets/Script/GrowthManager.cs
@@ -15,16 +15,28 @@ public class GrowthManager : MonoBehaviour
     void Update()
     {
         time += Time.deltaTime;
-        if(time > 5.0f)
+        if(time > 2.0f)
         {
             time = 0;
             if (transform.name.StartsWith("Cabbage") && transform.localScale.x < 1.0f)
             {
                 transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                Debug.Log("wow : " + transform.localScale.x);
+                if(transform.localScale.x >= 1.0f)
+                {
+                    Debug.Log("normalement c bon");
+                    gameObject.tag = "Selectable";
+                } 
             }
             else if(transform.name.StartsWith("Tomato") && transform.localScale.x < 2.0f)
             {
                 transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
+                Debug.Log("ah ouais : " + transform.localScale.x);
+                if (transform.localScale.x >= 2.0f)
+                {
+                    Debug.Log("c bon la ?");
+                    gameObject.tag = "Selectable";
+                }
             }
         }
     }
