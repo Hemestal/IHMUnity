482b04aa8ba5d6924c66b66c517a43f05dd96722
Hemestal <jetetroll45@gmail.com>
2021-02-24 14:10:17 +0100

    Sellfruits finished

--- ProjetIHM/Assets/Script/SellFruit.cs
+++ ProjetIHM/Assets/Script/SellFruit.cs
@@ -4,9 +4,43 @@ using UnityEngine;
 
 public class SellFruit : MonoBehaviour
 {
+    public static bool SellTomato, SellCabbage=false;
     // Start is called before the first frame update
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider player)
     {
-       //if 
+        //Debug.Log(player.gameObject.transform.parent.gameObject.name);
+        try
+        {
+            if (player.gameObject.transform.parent.gameObject.name == "Box_01")
+            {
+                SellTomato = true;
+            }
+            if (player.gameObject.transform.parent.gameObject.name == "Box_02")
+            {
+                SellCabbage = true;
+            }
+        }
+        catch
+        {
+            
+        }
+    }
+    private void OnTriggerExit(Collider player)
+    {
+        try
+        {
+            if (player.gameObject.transform.parent.gameObject.name == "Box_01")
+            {
+                SellTomato = false;
+            }
+            if (player.gameObject.transform.parent.gameObject.name == "Box_02")
+            {
+                SellCabbage = false;
+            }
+        }
+        catch
+        {
+
+        }
     }
 }
